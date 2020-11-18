c8afa57c8fbaaf72d84345d37968e3d16cf8a8e3
Alex Duval <alexduval71@gmail.com>
2020-11-18 15:41:34 +0100

    vue 101

---
+++ vue-1/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container">
+
+    <!-- <img :src="imgUrl" alt=""> -->
+
+
+    <h1>LOGIN APP</h1>
+    <div class="form-input">
+      <label for="email">Email</label>
+      <input type="text" v-model="email">
+    </div>
+    <div class="form-input">
+      <label for="password">Password</label>
+      <input type="password" v-model="password">
+    </div>
+    <div class="form-input">
+      <label for="firstName">firstName</label>
+      <input type="text" v-model="firstName"> <span :hidden="fieldsAreBlank">{{firstName.length }}</span>
+    </div>
+    <div class="form-input">
+      <label for="age">Age</label>
+
+
+      <input type="number" v-model="age">
+
+
+    </div>
+ {{ ageDouble}}
+      <p v-if="isMajor" class="majeur">
+      vous etes majeur
+      </p>
+      <p v-else class="mineur"> vous etes mineur</p>
+
+    <div class="form-input">
+      <button :disabled="fieldsAreBlank" @click="displayAlert()">Sign up</button>
+      <button @click="resetAllFields">RESET</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+      firstName: '',
+      age: 0,
+      imgUrl: 'https://web2.hirez.com/paladins//wp-content/uploads/2018/02/paladins-new-store.jpg'
+    };
+  },
+  methods: {
+    displayAlert() {
+      alert(`INFOS === EMAIL: ${this.email} === MDP:${this.password} `)
+    },
+    resetAllFields(){
+      this.email= '',
+      this.password= '',
+      this.firstName= '',
+      this.age= 0
+    },
+  },
+  computed: {
+    isMajor() {
+      return this.age > 17 ? true : false
+    },
+    ageDouble() {
+      return this.age * 2
+    },
+    fieldsAreBlank(){
+      return (this.firstName && this.email && this.password) === '' ? true : false
+    }
+  },
+  watch: {
+    age(value){
+      if (value < 0) {
+        this.age = 0
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 978px;
+  margin: auto;
+  text-align: center;
+}
+
+.form-input {
+  margin-bottom: 20px;
+}
+.form-input label {
+  margin-right: 10px;
+}
+
+.majeur {
+  background-color: green;
+  color: white;
+}
+.mineur {
+  background-color: red;
+  color: white;
+}
+</style>
